3cdc72dff26e75b840b800949b53dadabac465b9
Taylor Doyle <tcd56@cornell.edu>
2019-04-19 00:28:09 +0000

    Automatically backed up by Learn

--- levitation_quiz.rb
+++ levitation_quiz.rb
@@ -1,10 +1,9 @@
 
 def levitation_quiz
-  loop do 
+  loop do
       puts "What is the spell that enacts levitation?"
       answer = gets.chomp
       break if answer == "Wingardium Leviosa"
   end
   puts "You passed the quiz!"
 end
-
